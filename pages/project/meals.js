677b8826ace965c9b6d6b7c026a0965eb110c077
zero0914 <208417146@gms.tku.edu.tw>
2021-06-08 23:31:15 +0800

    make 3rd project

--- pages/project/meals.js
+++ pages/project/meals.js
@@ -1,15 +1,36 @@
 import styles from '../../styles/meals.module.css';
 import Head from 'next/head';
-import mealData from './meal-data'
+import { useState} from 'react';
+
 
 export default function meals() {
-  return (
+    
+    const [input, setInput] = useState('')
+    const [mealData, setMealData] = useState([])
+    
+    const handleInput =(e) => {
+        setInput(e.target.value)
+    }
+    console.log(input);
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
+        .then(res => res.json())
+        .catch(error => console.error('Error',error))
+        .then(res => {
+            console.log(res)
+            setMealData(res.meals)
+        })
+    }
+
+    return (
     <div>
       <Head>
         <link
           rel='stylesheet'
           href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'
-        />
+        onChange={handleInput}/>
         <title>Meal</title>
       </Head>
       <div className={styles.container}>
@@ -18,11 +39,12 @@ export default function meals() {
           <form id='submit' className={styles['flex-box']}>
             <input
               type='text'
-              id='search'
+              value={input}
               placeholder='Search for meals or keywords'
               className={styles.input}
+              onChange={handleInput}
             />
-            <button className={styles.btn__search} type='submit'>
+            <button className={styles.btn__search} type='submit' onClick={handleSubmit}>
               <i className='fas fa-search'></i>
             </button>
           </form>
@@ -31,11 +53,11 @@ export default function meals() {
           </button>
         </div>
         <div id='result-heading' className={styles.title__search}>
-          <h1>Search Results for ' beef ' :</h1>
+          <h1>Search Results for ' {input} ' :</h1>
         </div>
         <div id='meals' className={styles.gallery}>
           {mealData.map((meal)=>(
-              <Meal key={meal.id} name={meal.name} image={meal.image} />
+              <Meal key={meal.idMeal} name={meal.strMeal} image={meal.strMealThumb} />
           ))}
         </div>
       </div>
